d72eb141c6204e214519dbfaea487e0289c004eb
felipeperassi <fperassi@fi.uba.ar>
2024-09-18 15:51:01 -0300

    formato de graficos + graficos

--- TP1/Punto1a.m
+++ TP1/Punto1a.m
@@ -30,7 +30,7 @@ title('Histograma de Z_1');
 y1 = normpdf(x1,MeanZ1,sqrt(VarZ1));
 
 plot(x1,y1,'Color', '#75234a', 'LineWidth', 1.25);  
-legend('Histograma de Z_1','Distribución Normal Z_1');
+legend('Histograma de Z_1', sprintf('Z_1 \\sim N(\\mu = %.1e, \\sigma^{2} = %.2f)', MeanZ1, VarZ1));
 
 subplot(2,1,2)
 histogram(Z2, bines, 'Normalization', 'pdf', 'FaceColor', '#a9e2ac'); 
@@ -39,14 +39,13 @@ y2 = normpdf(x2,MeanZ2,sqrt(VarZ2));
 
 title('Histograma de Z_2');
 plot(x2, y2, 'Color', '#4a893d', 'LineWidth', 1.25);
-legend('Histograma de Z_2','Distribución Normal Z_2');
+legend('Histograma de Z_2', sprintf('Z_2 \\sim N(\\mu = %.1e, \\sigma^{2} = %.2f)', MeanZ2, VarZ2));
 
+coeficiente = corrcoef(Z1,Z2);
 
 figure();
 scatter(Z1,Z2, 'MarkerEdgeColor', '#0c3fc8');
 title('Gráfico de Dispersión: Z_2 vs Z_1');
+subtitle(sprintf('Coeficiente de Correlación de Pearson: \\rho = %.1e', coeficiente(1,2)));
 xlabel('Z_1');
 ylabel('Z_2');
-
-coeficiente = corrcoef(Z1,Z2);
-
