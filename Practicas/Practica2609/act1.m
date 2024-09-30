94317a5a32efa67050a488ac5dd7d5b37c01543d
MaximilianoRodriguez01 <masrodriguez@fi.uba.ar>
2024-09-30 17:30:48 -0300

    Correccion de practicas

--- Practicas/Practica2609/act1.m
+++ Practicas/Practica2609/act1.m
@@ -1,3 +1,9 @@
+% Instalar y cargar el paquete de imágenes en Octave
+if exist('OCTAVE_VERSION', 'builtin')
+    pkg install -forge image;
+    pkg load image;
+end
+
 clear variables
 close all
 
@@ -24,15 +30,6 @@ cov_X = cov(X');
 
 Y = Px' * X;
 
-
-figure();
-scatter(X(1,:), X(2,:));
-title('Original');
-
-figure();
-scatter(Y(1,:), Y(2,:));
-title('Transformada');
-
 %Ordeno autovectores y autovalores
 [Lambda_sorted, idx] = sort(diag(LAMBDA), 'descend');
 Px_sorted = Px(:, idx);
@@ -49,8 +46,19 @@ YR = V'*(X - mean(X));
 XR = V*YR + mean(X);
 
 %Rearmar imagen
-figure();
+
+figure(1);
+scatter(X(1,:), X(2,:));
+title('Original');
+
+figure(2);
+scatter(Y(1,:), Y(2,:));
+title('Transformada');
+
+figure(3);
 [fil, col] = size(img_gris);
 img_out = reshape(XR, [fil col]);
-
 imshow(uint8(img_out));
+title("Foto Comprimida");
+
+saveas(gcf, 'img_out.jpg');
