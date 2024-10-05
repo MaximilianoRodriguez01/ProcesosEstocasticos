e72fc994ccc47618a39713e10601867128e7be54
MaximilianoRodriguez01 <masrodriguez@fi.uba.ar>
2024-10-05 20:00:49 -0300

    ProcesoDePoisson

--- Practicas/Practica0310/act3.m
+++ Practicas/Practica0310/act3.m
@@ -18,8 +18,35 @@ for i = 1:m
     X(i, :) = Xi;
 end
 
+
+% Grafico las primeras 5 Binomiales para ver cómo tiene que ser el Proceso de Poisson
+figure();
 for i = 1:5
-    stem(X(i, :));
-    hold on;
+    subplot(5, 1, i);
+    plot(X(i, :));
+    title(['Bernoulli de orden ', num2str(i)]);
+end
+
+Poisson = zeros(5, n);
+
+for i = 1:5
+    for j = 1:n
+        if X(i, j) == 1 && j == 0
+            Poisson(i, j) = 1;
+        end
+        if X(i, j) == 1
+            Poisson(i, j) = Poisson(i, j-1) + 1;
+        elseif j > 1
+            Poisson(i, j) = Poisson(i, j-1);
+        end
+    end
+end
+
+% Grafico las primeras 5 Poisson
+figure();
+for i = 1:5
+    subplot(5, 1, i);
+    plot(Poisson(i, :));
+    title(['Poisson de orden ', num2str(i)]);
 end
 
