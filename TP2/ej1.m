804e48ab4e92bad66ff9f53c6ff1e1931c28c89f
MaximilianoRodriguez01 <masrodriguez@fi.uba.ar>
2024-10-10 13:19:28 -0300

    ej1 tp2

--- TP2/ej1.m
+++ TP2/ej1.m
@@ -10,21 +10,20 @@ tiempo_total = 1.64e9;
 
 tau = diff(instante_detectado);
 
-
 esperanza_tau = mean(tau);
 varianza_tau = var(tau);
 
-% x = linspace(0, max(tau), 100);
-% exp = exppdf(x, esperanza_tau);
+x = linspace(0, max(tau), 100);
+exp = exppdf(x, esperanza_tau);
 
-% % Histograma de los tau
-% figure(1);
-% histogram(tau, 'Normalization', 'pdf', 'FaceColor', '#ffd0ff');
-% title('Histograma de los intervalos entre detecciones');
-% subtitle(sprintf('Esperanza: %.2f, Varianza: %.2f', esperanza_tau, varianza_tau));
-% hold on
-% plot(x, exp, 'LineWidth', 2, 'Color', '#ff00ff');
-% hold off
+% Histograma de los tau
+figure(1);
+histogram(tau, 'Normalization', 'pdf', 'FaceColor', '#ffd0ff');
+title('Histograma de los intervalos entre detecciones');
+subtitle(sprintf('Esperanza: %.2f, Varianza: %.2f', esperanza_tau, varianza_tau));
+hold on
+plot(x, exp, 'LineWidth', 2, 'Color', '#ff00ff');
+hold off
 
 
 
