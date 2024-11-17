dd5e57d6883b52987c397d559f19f47ad89b2ba0
MaximilianoRodriguez01 <masrodriguez@fi.uba.ar>
2024-11-17 12:49:30 -0300

    Ej3 desplazado con E y SH

--- TP3/TP3.m
+++ TP3/TP3.m
@@ -40,89 +40,80 @@ v1 = v1';
 v2 = v2';
 v3 = v3';
 
-P1 = mean(abs(v1).^2);
-P2 = mean(abs(v2).^2);
-P3 = mean(abs(v3).^2);
+Ej_3(e, Fs, a(:,2), G(1,2), n_fft, M, K, L, v1, v2, v3);
+Ej_3(sh, Fs, a(:,8), G(1,8), n_fft, M, K, L, v1, v2, v3);
 
+function Ej_3(e, Fs, a, G, n_fft, M, K, L, v1, v2, v3)
+    P1 = mean(abs(v1).^2);
+    P2 = mean(abs(v2).^2);
+    P3 = mean(abs(v3).^2);
 
-% Divido la señal en segmentos de M muestras
-% Multiplico la señal divida en segmentos por la señal al
-% igual que el metodo de welch
+    E_n_10 = zeros(L(1), M(1));
+    E_n_100 = zeros(L(2), M(2));
+    E_n_1000 = zeros(L(3), M(3));
 
-E_n_10 = zeros(L(1), M(1));
-E_n_100 = zeros(L(2), M(2));
-E_n_1000 = zeros(L(3), M(3));
+    for i = 1:L(1)
+        E_n_10(i,:) = e(1, (i-1)*K(1)+1 : (i-1)*K(1) + M(1)).*v1(:,1);
+    end
 
+    for i = 1:L(2)
+        E_n_100(i,:) = e(1, (i-1)*K(2)+1 : (i-1)*K(2) + M(2)).*v2(:,1);
+    end
 
-for i = 1:L(1)
-    E_n_10(i,:) = x(1, (i-1)*K(1)+1 : (i-1)*K(1) + M(1)).*v1(:,1);
+    for i = 1:L(3)
+        E_n_1000(i,:) = e(1, (i-1)*K(3)+1 : (i-1)*K(3) + M(3)).*v3(:,1);
+    end
+
+    S_E_n_10 = zeros(L(1), n_fft);
+    S_E_n_100 = zeros(L(2), n_fft);
+    S_E_n_1000 = zeros(L(3), n_fft);
+
+    for i = 1:L(1)
+        S_E_n_10(i,:) = PSD(E_n_10(i,:)*v1(:,1), M(1)*P1, n_fft);
+    end
+    for i = 1:L(2)
+        S_E_n_100(i,:) = PSD(E_n_100(i,:)*v2(:,1), M(2)*P2, n_fft);
+    end
+    for i = 1:L(3)
+        S_E_n_1000(i,:) = PSD(E_n_1000(i,:)*v3(:,1), M(3)*P3, n_fft);
+    end
+
+    S_E_n_10_prom = mean(S_E_n_10);
+    S_E_n_100_prom = mean(S_E_n_100);
+    S_E_n_1000_prom = mean(S_E_n_1000);
+
+    He = freqz(G, a, n_fft, 'whole', Fs);
+    S_E = abs(He).^2;
+
+    figure();
+    f = linspace(-Fs/2, Fs/2, n_fft);
+    plot(f, 20*log10(S_E_n_10_prom), 'LineWidth', 1.2, 'DisplayName', 'Estimada');
+    hold on;
+    plot(f, 20*log10(S_E), 'DisplayName', 'Teórica');
+    xlim([0 Fs/2])
+    xlabel('Frecuencia [Hz]');
+    ylabel('Amplitud [dB]');
+
+    figure();
+    plot(f, 20*log10(S_E_n_100_prom), 'LineWidth', 1.2, 'DisplayName', 'Estimada');
+    hold on
+    plot(f, 20*log10(S_E), 'DisplayName', 'Teorica');
+    xlim([0 Fs/2]);
+    xlabel('Frecuencia [Hz]');
+    ylabel('Amplitud [dB]');
+    title('PSD');
+    legend('Location', 'South', 'Orientation', 'Horizontal');
+
+    figure();
+    plot(f, 20*log10(S_E_n_1000_prom), 'LineWidth', 1.2, 'DisplayName', 'Estimada');
+    hold on
+    plot(f, 20*log10(S_E), 'DisplayName', 'Teorica');
+    xlim([0 Fs/2]);
+    xlabel('Frecuencia [Hz]');
+    ylabel('Amplitud [dB]');
+    title('PSD');
+    legend('Location', 'South', 'Orientation', 'Horizontal');
 end
-
-for i = 1:L(2)
-    E_n_100(i,:) = x(1, (i-1)*K(2)+1 : (i-1)*K(2) + M(2)).*v2(:,1);
-end
-
-for i = 1:L(3)
-    E_n_1000(i,:) = x(1, (i-1)*K(3)+1 : (i-1)*K(3) + M(3)).*v3(:,1);
-end
-
-% Calculo la PSD de cada segmento
-S_E_n_10 = zeros(L(1), n_fft);
-S_E_n_100 = zeros(L(2), n_fft);
-S_E_n_1000 = zeros(L(3), n_fft);
-
-for i = 1:L(1)
-    S_E_n_10(i,:) = PSD(E_n_10(i,:)*v1(:,1), M(1)*P1, n_fft);
-end
-for i = 1:L(2)
-    S_E_n_100(i,:) = PSD(E_n_100(i,:)*v2(:,1), M(2)*P2, n_fft);
-end
-for i = 1:L(3)
-    S_E_n_1000(i,:) = PSD(E_n_1000(i,:)*v3(:,1), M(3)*P3, n_fft);
-end
-
-% Calculo la PSD promedio
-S_E_n_10_prom = mean(S_E_n_10);
-S_E_n_100_prom = mean(S_E_n_100);
-S_E_n_1000_prom = mean(S_E_n_1000);
-
-He = freqz(G(1), a(:,1), n_fft, 'whole', Fs); 
-S_E = abs(He).^2; 
-
-% Grafico
-figure();
-f = linspace(-Fs/2, Fs/2, n_fft);
-plot(f, 20*log10(S_E_n_10_prom), 'LineWidth', 1.2, 'DisplayName', 'Estimada');
-hold on;
-plot(f, 20*log10(S_E), 'DisplayName', 'Teórica');
-xlim([0 Fs/2])
-xlabel('Frecuencia [Hz]');
-ylabel('Amplitud [dB]');
-title('PSD');
-legend('Location', 'South', 'Orientation', 'Horizontal');
-
-figure();
-plot(f, 20*log10(S_E_n_100_prom), 'LineWidth', 1.2, 'DisplayName', 'Estimada');
-hold on
-plot(f, 20*log10(S_E), 'DisplayName', 'Teorica');
-
-xlim([0 Fs/2]);
-xlabel('Frecuencia [Hz]');
-ylabel('Amplitud [dB]');
-title('PSD');
-legend('Location', 'South', 'Orientation', 'Horizontal');
-
-figure();
-plot(f, 20*log10(S_E_n_1000_prom), 'LineWidth', 1.2, 'DisplayName', 'Estimada');
-hold on
-plot(f, 20*log10(S_E), 'DisplayName', 'Teorica');
-xlim([0 Fs/2]);
-xlabel('Frecuencia [Hz]');
-ylabel('Amplitud [dB]');
-title('PSD');
-legend('Location', 'South', 'Orientation', 'Horizontal');
-
-
 
 function f = Ej_2(x, Fs, a, G, n_fft)
     
